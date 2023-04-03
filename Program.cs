c900dac55235ffb644f2e453bd85abdfab7f1787
Veeralii <119738164+Veeralii@users.noreply.github.com>
2023-04-03 11:24:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uusimuistipeli
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMuistipeli1());
+            Application.Run(new FrmMuistipeli2());
+            
+        }
+    }
+}
