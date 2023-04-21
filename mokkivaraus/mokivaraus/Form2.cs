7966c8c31f107ecf66d281a0dd1d1689799513d5
Kouhiaa <lauri_kouhi@hotmail.com>
2023-04-21 11:27:10 +0300

    Mökin lisää formi tehty

--- mokkivaraus/mokivaraus/Form2.cs
+++ mokkivaraus/mokivaraus/Form2.cs
@@ -16,5 +16,35 @@ namespace mokivaraus
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
